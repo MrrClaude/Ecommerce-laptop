1db6af957542babbd31d7e19c01b25be06656cb9
MrrClaude <nhemsoksophorn@gamil.com>
2025-10-06 19:57:57 +0700

    third push

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,10 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { IoMoon, IoSunny } from "react-icons/io5";
+import { FiShoppingCart } from "react-icons/fi";
 import ThemeContext from "../context/themeProvder";
 import imgLogo from "/Artboard1.png";
 import Login from "./Login";
-import { FiShoppingCart } from "react-icons/fi";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
@@ -13,27 +14,29 @@ const Navbar = () => {
 
   const { theme, toggle } = useContext(ThemeContext);
   const [showNavbar, setShowNavbar] = useState(true);
-  const [lastScrollY, setLastScrollY] = useState(0);
-  useEffect(() => {
-    const handleScroll = () => {
-      const currentScrollY = window.scrollY;
-      if (currentScrollY === 0) {
-        setShowNavbar(true);
-      } else if (currentScrollY > lastScrollY + 5) {
-        setShowNavbar(true);
-      } else if (currentScrollY < lastScrollY - 5) {
-        setShowNavbar(false);
-      }
 
-      setLastScrollY(currentScrollY);
+  useEffect(() => {
+    let scrollTimeout;
+
+    const handleScroll = () => {
+      setShowNavbar(false);
+
+      if (scrollTimeout) clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(() => {
+        setShowNavbar(true);
+      }, 250);
     };
 
     window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, [lastScrollY]);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      if (scrollTimeout) clearTimeout(scrollTimeout);
+    };
+  }, []);
+
   return (
-    <div className="relative ">
+    <div className="relative">
       <nav
         className={`fixed top-0 left-0 right-0 z-50 transition-transform duration-300 ease-in-out
         ${
@@ -52,7 +55,7 @@ const Navbar = () => {
             <img src={imgLogo} className="h-20" alt="Logo" />
             <span
               className={`self-center text-2xl font-bold ${
-                theme === "dark" ? " text-indigo-700" : "text-black"
+                theme === "dark" ? "text-indigo-700" : "text-black"
               }`}
             >
               ICT
@@ -62,7 +65,7 @@ const Navbar = () => {
           <div className="flex items-center space-x-3 md:order-2">
             <button
               onClick={() => setIsSearchOpen(!isSearchOpen)}
-              className="md:hidden p-2 rounded-lg "
+              className="md:hidden p-2 rounded-lg"
             >
               🔍
             </button>
@@ -104,7 +107,7 @@ const Navbar = () => {
 
             <button
               onClick={() => setIsMenuOpen(!isMenuOpen)}
-              className="md:hidden p-2 hover:bg-gray-100  rounded-lg"
+              className="md:hidden p-2 hover:bg-gray-100 rounded-lg"
             >
               ☰
             </button>
@@ -115,7 +118,7 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="Search..."
-                className="block w-full md:w-64 p-2 pl-3 text-sm border border-gray-300 rounded-lg  focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600"
+                className="block w-full md:w-64 p-2 pl-3 text-sm border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600"
               />
             </div>
           )}
@@ -125,7 +128,7 @@ const Navbar = () => {
               isMenuOpen ? "block" : "hidden"
             } w-full md:flex md:w-auto md:order-1`}
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg  md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent md:dark:bg-transparent">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent md:dark:bg-transparent">
               <li>
                 <Link
                   to="/"
@@ -152,20 +155,20 @@ const Navbar = () => {
                 </button>
                 {isDropdownOpen && (
                   <div
-                    className={`absolute left-0 mt-2 w-64   border rounded-lg shadow-lg ${
+                    className={`absolute left-0 mt-2 w-64 border rounded-lg shadow-lg ${
                       theme === "dark"
                         ? "bg-gradient-to-tr from-gray-900 via-gray-950 to-black text-white border-t border-gray-700"
                         : "bg-white border-gray-200"
                     }`}
                   >
                     <ul
-                      className="grid grid-cols-2 gap-2 p-3  "
+                      className="grid grid-cols-2 gap-2 p-3"
                       onClick={() => setIsDropdownOpen(false)}
                     >
                       <li>
                         <Link
                           to="/gaming"
-                          className="block p-2 rounded hover:text-blue-700 "
+                          className="block p-2 rounded hover:text-blue-700"
                         >
                           Gaming-laptop
                         </Link>
@@ -173,7 +176,7 @@ const Navbar = () => {
                       <li>
                         <Link
                           to="/business"
-                          className="block p-2 rounded hover:text-blue-700 "
+                          className="block p-2 rounded hover:text-blue-700"
                         >
                           Business-laptop
                         </Link>
@@ -181,7 +184,7 @@ const Navbar = () => {
                       <li>
                         <Link
                           to="/accessories"
-                          className="block p-2 rounded hover:text-blue-700 "
+                          className="block p-2 rounded hover:text-blue-700"
                         >
                           Accessories
                         </Link>
@@ -210,6 +213,7 @@ const Navbar = () => {
           </div>
         </div>
       </nav>
+
       <Login isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
     </div>
   );
